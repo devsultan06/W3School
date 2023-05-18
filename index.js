9ee7157f9db08a0fe20dc37fd855d5ace49a9003
Sultan Omo <sultanabaniks@gmail.com>
2023-05-18 13:34:54 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const header = document.querySelector("#header");
+const background = document.querySelector("#background");
+const links = document.querySelectorAll(".link");
+
+
+
+
+
+
+background.addEventListener("click", () => {
+    header.classList.toggle("active");
+
+    links.forEach((linked) => {
+        linked.classList.toggle("active");
+
+    })
+
+
+ 
+})
+
+
