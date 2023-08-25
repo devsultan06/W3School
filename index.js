11a05341672c15eecde09e34820c20c7bc6919e2
Pro-Sultan <sultanabaniks@gmail.com>
2023-08-25 16:50:10 -0700

    first commit

--- index.js
+++ index.js
@@ -2,21 +2,10 @@ const header = document.querySelector("#header");
 const background = document.querySelector("#background");
 const links = document.querySelectorAll(".link");
 
-
-
-
-
-
 background.addEventListener("click", () => {
-    header.classList.toggle("active");
+  header.classList.toggle("active");
 
-    links.forEach((linked) => {
-        linked.classList.toggle("active");
-
-    })
-
-
- 
-})
-
-
+  links.forEach((linked) => {
+    linked.classList.toggle("active");
+  });
+});
